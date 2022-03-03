cf9a1e9a983c3b32cf7290d7ad038107344eec88
jooe <ojooeo@gmail.com>
2022-03-03 17:22:54 +0800

    .

--- src/views/News/index.vue
+++ src/views/News/index.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
+import { newsList } from './news'
 
 // 生命周期
 onMounted(() => {
@@ -34,27 +35,18 @@ const goPage = (page = '/') => {
     <div class="max-width content">
       <n-breadcrumb>
         <n-breadcrumb-item @click="goPage('/')"> 首页 </n-breadcrumb-item>
-        <n-breadcrumb-item> 关于我们-丝路银行简介 </n-breadcrumb-item>
+        <n-breadcrumb-item> 关于我们-新闻动态 </n-breadcrumb-item>
       </n-breadcrumb>
     </div>
-    <div class="max-width content">
-      <div class="cont">
-        <div class="title">丝路国际银行简介</div>
-        <ul class="list">
-          <li>
-            <span>丝路国际银行（Silkroad International Bank，简称SIB）</span>
-            位于吉布提共和国首都吉布提市，由招商局、丝路亿商等中资企业和吉布提政府共同发起，经吉布提央行批准成立的吉布提第13家商业银行;
-            是中国“一带一路”战略在非洲落地的重要金融基础设施，也是中非在金融领域的务实合作成果之一。
-          </li>
-          <li>
-            <span>丝路国际银行</span>
-            秉承“连通丝绸之路，共创东非辉煌”的经营理念，坚持“立足吉布提，服务东非，连接中国，面向全球”的发展定位，助力吉布提打造成为“东非的金融中心，非洲之角的迪拜”。
-          </li>
-          <li>
-            <span>丝路国际银行</span>
-            以“科学、稳健、协调发展”为根本，不断开拓市场，深化金融创新，致力成为多项银行业务领域的市场领先者，致力于为吉布提人民及在吉布提的外资企业提供良好的金融服务。
-          </li>
-        </ul>
+
+    <div>
+      <div class="max-width content" v-for="item in newsList" :key="item.id">
+        <div class="title">{{ item.title }}</div>
+        <div class="date">
+          <span>{{ item.date }}</span>
+          <span>{{ item.user }}</span>
+        </div>
+        <n-ellipsis line-clamp="3" :tooltip="false"> {{ item.content }} </n-ellipsis>
       </div>
     </div>
   </div>
@@ -75,53 +67,50 @@ const goPage = (page = '/') => {
     background-color: white;
     padding: 20px;
     border-radius: 6px;
-    .cont {
-      .title {
-        font-size: 18px;
-        position: relative;
-        padding-bottom: 5px;
-        &::after {
-          content: '';
-          width: 2em;
-          height: 4px;
-          border-radius: 2px;
-          background-color: rgba(0, 188, 150, 0.9);
-          position: absolute;
-          bottom: 0;
-          left: 0;
-          display: inline-block;
-          z-index: 3;
-        }
-        &::before {
-          content: '';
-          width: 100%;
-          height: 4px;
-          border-radius: 2px;
-          background-color: rgba(196, 196, 196, 0.1);
-          position: absolute;
-          bottom: 0;
-          left: 0;
-          display: inline-block;
-          z-index: 2;
-        }
+    cursor: pointer;
+    &:hover {
+      background-color: rgba(0, 188, 150, 0.05);
+    }
+    .title {
+      font-size: 22px;
+      position: relative;
+      padding-bottom: 5px;
+      &::after {
+        content: '';
+        width: 100%;
+        height: 2px;
+        border-radius: 2px;
+        background-color: rgba(0, 188, 150, 0.9);
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        display: inline-block;
+        z-index: 3;
       }
-      .list {
-        list-style: none;
-        padding: 20px;
-        li {
-          font-size: 16px;
-          color: #666;
-          padding-bottom: 20px;
-          &:last-child {
-            padding-bottom: 0;
-          }
-          span {
-            font-weight: 700;
-            color: #333;
-            line-height: 1.6;
-          }
-        }
+      &::before {
+        content: '';
+        width: 100%;
+        height: 2px;
+        border-radius: 2px;
+        background-color: rgba(196, 196, 196, 0.1);
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        display: inline-block;
+        z-index: 2;
       }
+    }
+    .date {
+      padding: 5px 0;
+      span {
+        margin-right: 20px;
+        color: rgba(0, 188, 150, 0.9);
+      }
+    }
+    .n-ellipsis {
+      font-size: 16px;
+      text-align: justify;
+      color: #666;
     }
   }
 }
